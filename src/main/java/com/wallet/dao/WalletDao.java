20bbc1027278b648fcd9ccb2cbd524714a66256b
Sunny <sunnysinghhrana@gmail.com>
2026-03-06 22:40:14 +0530

    Add Model classes + DBConnection class

---
+++ src/main/java/com/wallet/dao/WalletDao.java
@@ -0,0 +1,15 @@
+package com.wallet.dao;
+
+import com.wallet.util.DBConnection.DBConnection;
+import com.wallet.model.Wallet;
+import java.math.BigDecimal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class WalletDao {
+
+}
